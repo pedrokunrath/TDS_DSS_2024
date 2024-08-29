6fedb48ca04ec10827181a8c9309ff39e223d8dd
pedro <151687035+pedrokunrath@users.noreply.github.com>
2024-08-28 22:13:10 -0300

    .

--- Scripts/server.js
+++ Scripts/server.js
@@ -1,8 +1,9 @@
 const list = [1,3,4,7,2,6,9,0,8,5];
 
-function Oderna_lista(){
-   
-    console.log(list);
-    return 0;
+function Odernalista(parametro){
+    return parametro.sort();
 } 
-var oderna = Oderna_list();
+
+var oderna = Odernalista(list);
+console.log('essa é a lista anterior : ' + list);
+console.log('essa é a lista ordenada : ' + oderna);
